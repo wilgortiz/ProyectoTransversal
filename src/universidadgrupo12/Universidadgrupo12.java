aa36b4a39abeef5168beaaad1656d8f5cff495db
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-25 13:49:51 -0300

    Acomode main

--- src/universidadgrupo12/Universidadgrupo12.java
+++ src/universidadgrupo12/Universidadgrupo12.java
@@ -20,39 +20,71 @@ import modelo.Materia;
 public class Universidadgrupo12 {
 
     public static void main(String[] args) {
-
-        //instancia de alumno y alumnodata
-        Alumno al = new Alumno(7, "30258697", "koka", "martin", LocalDate.parse("1986-12-05"), true);
+ //PRUEBA DE ALUMNO
+        System.out.println("PRUEBA DE ALUMNO:");
+        Alumno al = new Alumno(1, "21589754", "pascual", "darius", LocalDate.parse("1980-12-12"), true);
         AlumnoData alD = new AlumnoData();
         alD.guardarAlumno(al);
-        //instancia de materia y materiadata
-        MateriaData md = new MateriaData();
-        Materia m1 = new Materia(4, "LENGUA", 3, true);
-        md.guardarMateria(m1);
+        System.out.println("Alumno creado y guardado con exito en BD");
+        System.out.println("Buscar Alumno id=1:");
+        System.out.println(alD.buscarAlumno(1).toString());
       
-        Materia m2 = new Materia(6, "Algebra", 1, false);
-        md.guardarMateria(m2);
-       
-        System.out.println(md.buscarMateria(3).toString());
-
-        //modificar estado
-        md.modificarEstado(2); 
         
-        //instancia de Inscripcion e InscripcionData
+        //PRUEBA DE MATERIA
+        System.out.println("PRUEBA DE MATERIA:");
+        MateriaData md = new MateriaData();
+        Materia m1 = new Materia(1, "Algebra", 1, true);
+        md.guardarMateria(m1);
+        Materia m2 = new Materia(1, "Ingles", 2, true);
+        System.out.println("Materias creadas y guardadas con exito en BD");
+        System.out.println("Buscar materia id=1:");
+        System.out.println(md.buscarMateria(1).toString());
+        System.out.println("Modificar materia " + m1.getNombre()+ ":");
+        System.out.println("ahora es: " + md.modificarMateria(m2).toString());
+        System.out.println("Listar materias:" + md.listarMaterias().toString());
+        
+
+        //PRUEBA INSCRIPCION
+        System.out.println("PRUEBA INSCRIPCION:");
         Inscripcion ins = new Inscripcion(9, al, m1);
         InscripcionData insD = new InscripcionData();
         insD.inscribir(ins);
+        System.out.println("Inscripcion completada");
+        System.out.println("Inscipcion de aluno id=1:");
+        System.out.println(insD.inscripcionesPorAlumno(1).toString());
+//        //instancia de alumno y alumnodata
+//        Alumno al = new Alumno(7, "30258697", "koka", "martin", LocalDate.parse("1986-12-05"), true);
+//        AlumnoData alD = new AlumnoData();
+//        alD.guardarAlumno(al);
+//        //instancia de materia y materiadata
+//        MateriaData md = new MateriaData();
+//        Materia m1 = new Materia(4, "LENGUA", 3, true);
+//        md.guardarMateria(m1);
+//      
+//        Materia m2 = new Materia(6, "Algebra", 1, false);
+//        md.guardarMateria(m2);
+//       
+//        System.out.println(md.buscarMateria(3).toString());
+//
+//        //modificar estado
+//        md.modificarEstado(2); 
+//        
+//        //instancia de Inscripcion e InscripcionData
+//        Inscripcion ins = new Inscripcion(9, al, m1);
+//        InscripcionData insD = new InscripcionData();
+//        insD.inscribir(ins);
+//
+//        System.out.println("busca");
+//        System.out.println(md.buscarMateria(3).toString());
+//        System.out.println("modifica");
+//        System.out.println(md.modificarMateria(m2));
+//        System.out.println("lista de materias");
+//        System.out.println(md.listarMaterias().toString());
+//
+//        
+//        System.out.println("lista de inscriptos");
+//        System.out.println(insD.materiasInscripto(al).toString());
+//        //insD.materiasInscripto(al).toString();
 
-        System.out.println("busca");
-        System.out.println(md.buscarMateria(3).toString());
-        System.out.println("modifica");
-        System.out.println(md.modificarMateria(m2));
-        System.out.println("lista de materias");
-        System.out.println(md.listarMaterias().toString());
-
-        
-        System.out.println("lista de inscriptos");
-        System.out.println(insD.materiasInscripto(al).toString());
-        //insD.materiasInscripto(al).toString();
     }
 }
